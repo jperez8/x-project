dd3f42c0d4614bfc5de5c53f3f3b648bfc7374b6
jperez8 <jperez@rica.design>
2022-06-14 01:06:20 +0200

    feat: username collumn on profiles table

--- database/migrations/2022_03_22_153517_create_profiles_table.php
+++ database/migrations/2022_03_22_153517_create_profiles_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('type_profile_id')->constrained();
+            $table->text('username')->nullable();
             $table->text('description')->nullable();
             $table->string('profile_mini_image')->nullable();
             $table->string('profile_header_image')->nullable();
